fe7280d60c50eafe4cf2f811a0bc306d6100afc3
wyq <992260862@qq.com>
2020-06-10 16:38:46 +0800

    创建一个空文件

---
+++ learnMySQL/learnMySQL/learnMySQLDlg.h
@@ -0,0 +1,32 @@
+
+// learnMySQLDlg.h : 头文件
+//
+
+#pragma once
+
+
+// ClearnMySQLDlg 对话框
+class ClearnMySQLDlg : public CDialogEx
+{
+// 构造
+public:
+	ClearnMySQLDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_LEARNMYSQL_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
